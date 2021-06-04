9532b7993bbaf78ada1f5cfa31506d1e0c785a20
Jo-yong-hui <80802790+Jo-yong-hui@users.noreply.github.com>
2021-06-04 11:06:13 +0900

    spring tiles3 나머지설정

---
+++ java/yh/spring/mvc/joinController.java
@@ -0,0 +1,33 @@
+package yh.spring.mvc;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class joinController {
+
+	
+	@RequestMapping("/join/agree")
+	public String agree() {
+		return"join/agree.tiles";
+		
+	}
+	
+	@RequestMapping("/join/checkme")
+	public String checkme() {
+		return"join/checkme.tiles";
+		
+	}
+	@RequestMapping("/join/joinme")
+	public String joinme() {
+		return"join/joinme.tiles";
+		
+	}
+	@RequestMapping("/join/joinok")
+	public String joinok() {
+		return"join/joinok.tiles";
+		
+	}
+	
+	
+}
